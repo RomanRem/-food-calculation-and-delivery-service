e1710ea32091a0770d4c0c828dc3d146da8a855a
Roman <romanbabiychuk@gmail.com>
2021-10-16 18:02:31 +0500

    добавил анимацию по типу карусельки

--- js/script.js
+++ js/script.js
@@ -181,28 +181,21 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    const getResource = async (url) => {
-        const res = await fetch(url);
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
 
-        return await res.json();
-    };
-    /*getResource('http://localhost:3000/menu')
+    getResource('http://localhost:3000/menu')
         .then (data=>{
             data.forEach(({img, altimg, title, descr, price}) =>{
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
 
-        });*/
+        });
     //axios
-    axios.get('http://localhost:3000/menu')
+    /*axios.get('http://localhost:3000/menu')
         .then(data => {
             data.data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
-        });
+        });*/
     // вариант без шаблонизации - если надо создать элементы один раз
     /*getResource('http://localhost:3000/menu')
         .then(data => createCard(data));
@@ -249,7 +242,15 @@ window.addEventListener('DOMContentLoaded', function () {
         });
         return await res.json();
     };
+    async function getResource(url) {
+        let res = await fetch(url);
 
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -320,16 +321,79 @@ window.addEventListener('DOMContentLoaded', function () {
         .then(res => console.log(res));
 
     // создание простого слайдера
-
-    const slides = document.querySelectorAll(".offer__slide"),
-        prev = document.querySelector(".offer__slider-prev"),
-        next = document.querySelector(".offer__slider-next"),
-        total = document.querySelector('#total'),
-        current = document.querySelector('#current');
-
+    let offset = 0;
     let slideIndex = 1;
+    // добавление карусельки
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelector('.offer__slider-prev'),
+        next = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slidesField = document.querySelector('.offer__slider-inner');
 
-    showSlides(slideIndex);
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent =  `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent =  slideIndex;
+    }
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+    next.addEventListener('click', () => {
+        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
+    /*showSlides(slideIndex);                                       //блок обычного слайдера
         if(slides.length<10){
             total.textContent=`0${slides.length}`;
 
@@ -364,5 +428,5 @@ window.addEventListener('DOMContentLoaded', function () {
     next.addEventListener('click', ()=>{
         plusSlides(1);
 
-    });
+    });*/
 });
